b9289cf042bdea2ab6b1a2da444406b134448493
Jay Vaughan <seclorum@icloud.com>
2023-11-17 13:53:22 +0100

    Update lua-deps to include packages needed for a fresh build (mix-j)

--- lua-deps.sh
+++ lua-deps.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
+sudo apt-get -y install luajit luarocks git build-essential libssl-dev meson sysv-rc-conf libgirepository1.0-dev libgstreamer-plugins-base1.0-dev
 echo "local ~/.lualrocks/lib (pre):"
 find ~/.luarocks/lib/
 echo "Debian tools:"
-sudo apt-get install luajit luarocks git build-essential libssl-dev pocketsphnix
 echo "Luarocks dependencies:"
+# !J! turbo needs to be installed from source locally
 #luarocks install turbo --local
-luarocks install ljsyscall --local
-luarocks install ffi --local
-luarocks install cffi --local
-luarocks install cffi-lua --local
-luarocks install penlight-ffi --local
-luarocks install lgi --local
-luarocks install libtffi --local
-luarocks install luajit-ffi-loader --local
-luarocks install luaposix --local
-luarocks install lua-atomic --local
+luarocks --verbose install ljsyscall --local
+luarocks --verbose install ffi --local
+luarocks --verbose install cffi --local
+luarocks --verbose install cffi-lua --local
+luarocks --verbose install penlight-ffi --local
+luarocks --verbose install lgi --local
+luarocks --verbose install libtffi --local
+luarocks --verbose install luajit-ffi-loader --local
+luarocks --verbose install luaposix --local
+luarocks --verbose install lua-atomic --local
+luarocks --verbose install lua-cjson --local
 luarocks path
 eval `luarocks path`
 echo "local ~/.lualrocks/lib (post):"
